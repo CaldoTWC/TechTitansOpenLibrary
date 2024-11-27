c5e7541f00c7a1ab6f108c33a289619f31940293
Aldo Salazar <136004329+CaldoTWC@users.noreply.github.com>
2024-11-27 13:02:45 -0600

    Add files via upload

---
+++ TechTitansOpenLibrary/TechTitansOpenLibrary/src/main/java/com/TechTitans/TechTitansOpenLibrary/TechTitansOpenLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.TechTitans.TechTitansOpenLibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechTitansOpenLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechTitansOpenLibraryApplication.class, args);
+	}
+
+}
---
+++ TechTitansOpenLibrary/TechTitansOpenLibrary/src/test/java/com/TechTitans/TechTitansOpenLibrary/TechTitansOpenLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TechTitans.TechTitansOpenLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechTitansOpenLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
